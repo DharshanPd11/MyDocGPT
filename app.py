1815222ba16c5172f2cde790eeea4fc4a1610357
Dharshan_pd11 <101190088+DharshanPd11@users.noreply.github.com>
2023-10-27 21:00:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def get_text_chunks(text):
 
 def get_vectorstore(text_chunks):
     #embeddings = OpenAIEmbeddings()
-    embeddings = HuggingFaceBgeEmbeddings(model_name="BAAI/bge-small-en-v1.5")
+    embeddings = HuggingFaceBgeEmbeddings(model_name="hkunlp/instructor-large")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
