2c8cb4b5c141646beca0f4a319a7c649e7985d58
Dharshan_pd11 <101190088+DharshanPd11@users.noreply.github.com>
2023-11-05 18:59:48 +0530

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -28,7 +28,7 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Dchatbot&psig=AOvVaw0aFViAXGR_Gx0xZIZ43nv5&ust=1699277252303000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPjlxpj7rIIDFQAAAAAdAAAAABAF" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.chegos.pl%2Frobot-png-image-robot-png-robot-png-pp-13177140&psig=AOvVaw0aAA_nHfL7-DmepHPijB-g&ust=1699277339463000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLDAhsL7rIIDFQAAAAAdAAAAABAR" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
