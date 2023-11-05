d3400a6f01ce0f63c609284b3fe47a7cf77fde5e
Dharshan_pd11 <101190088+DharshanPd11@users.noreply.github.com>
2023-11-05 18:57:00 +0530

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -38,7 +38,6 @@ user_template = '''
 <div class="chat-message user">
     <div class="avatar">
         <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
-    </div>    ">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
