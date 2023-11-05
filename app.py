502c299605172d8c94d2071ef53de2715fa91466
Dharshan_pd11 <101190088+DharshanPd11@users.noreply.github.com>
2023-11-05 20:10:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def main():
                 flag = 0
     user_question = st.chat_input("Ex: What is the document about?")
     if user_question:
-        if (flag == 1):
+        if (flag == 0):
             st.error("No File")
         else:
             handle_userinput(user_question)
